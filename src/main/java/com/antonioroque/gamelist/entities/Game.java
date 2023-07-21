1af747ce16edeb8aeebd372c761ea86a678dfdfa
Antonio Carlos Roque <antoniocarlos.roque@gmail.com>
2023-07-21 06:17:42 -0300

    Seed H2

--- src/main/java/com/antonioroque/gamelist/entities/Game.java
+++ src/main/java/com/antonioroque/gamelist/entities/Game.java
@@ -18,20 +18,29 @@ public class Game {
 
   @Column(name = "game_year") // "year" is a reserved word in SQL
   private Integer year;
-  private String platform;
+  private String genre;
+  private String platforms;
+  private Double score;
   private String imgUrl;
+
+  @Column(columnDefinition = "TEXT")
   private String shortDescription;
+
+  @Column(columnDefinition = "TEXT")
   private String longDescription;
 
   public Game() {}
 
-  public Game(Long id, String title, Integer year, String platform, String imgUrl,
+  public Game(Long id, String title, Integer year, String genre, String platforms, Double score,
+      String imgUrl,
       String shortDescription,
       String longDescription) {
     this.id = id;
     this.title = title;
     this.year = year;
-    this.platform = platform;
+    this.genre = genre;
+    this.platforms = platforms;
+    this.score = score;
     this.imgUrl = imgUrl;
     this.shortDescription = shortDescription;
     this.longDescription = longDescription;
@@ -49,8 +58,16 @@ public class Game {
     return year;
   }
 
-  public String getPlatform() {
-    return platform;
+  public String getGenre() {
+    return genre;
+  }
+
+  public String getPlatforms() {
+    return platforms;
+  }
+
+  public Double getScore() {
+    return score;
   }
 
   public String getImgUrl() {
@@ -77,8 +94,16 @@ public class Game {
     this.year = year;
   }
 
-  public void setPlatform(String platform) {
-    this.platform = platform;
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public void setPlatforms(String platforms) {
+    this.platforms = platforms;
+  }
+
+  public void setScore(Double score) {
+    this.score = score;
   }
 
   public void setImgUrl(String imgUrl) {
